ecae50a4e7604771dab63594b6ee5d5f5f33c098
Jorge Abdo <jorgeabdo@Jorges-MacBook-Pro.local>
2020-10-20 17:51:13 -0500

    firebase connection

--- src/pages/dashboard/routes/homepage.jsx
+++ src/pages/dashboard/routes/homepage.jsx
@@ -1,28 +1,32 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Card, Button } from "react-bootstrap";
 import firebase from "firebase";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 import "../../../css/styles.css";
 
-const Homepage = () => {
-  let [artworks, setArtworks] = useState([]);
-
-  let artworks_list = [];
-
-  useEffect(() => {
-    loadArtworks();
-  }, []);
-
-  const loadArtworks = async () => {};
-
-  console.log("this is a way too see how the website works");
+const Homepage = ({ artwork }) => {
+  console.log(artwork);
   return (
     <React.Fragment>
       <div className="h2 text-center mb-4">Explore</div>
       <container-fluid className="item mx-4">
+        {artwork && artwork.map(({ name }) => <div>{name}</div>)}
         <div className="row equal-height little_margin"></div>
       </container-fluid>
     </React.Fragment>
   );
 };
 
-export default Homepage;
+const mapStateToProps = (state) => {
+  return {
+    artwork: state.firestore.ordered.artworks,
+    profile: state.firebase.profile,
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "artworks" }])
+)(Homepage);
