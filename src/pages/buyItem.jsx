3c023b11bbf7bf57d196fe129ceef737f2cfbb6c
Trevinno <Trevinno>
2020-10-25 22:53:28 -0500

    latest changes

--- src/pages/buyItem.jsx
+++ src/pages/buyItem.jsx
@@ -44,34 +44,3 @@ const BuyItem = () => {
 }
  
 export default BuyItem;
-
-
-
-
-
-{/* <body className="fullwidth">
-        <section className="wrap">
-            <section data-theme="default">
-                <section className="wrap">
-                <h1>Self-Responsive Order Form</h1>
-                <h2>With Responsive Styles Powered by Element Queries</h2>
-                <h2>Default Theme</h2>
-                <form>
-                <h3>Billing Information</h3>
-                <fieldset id="billingAddress">
-                <input type="text" name="cc_first_name" id="firstName" placeholder="First Name" autocorrect="off" />
-                <input type="text" name="cc_last_name" id="lastName" placeholder="Last Name" autocorrect="off" />
-                <input type="text" name="cc_street1" id="address" className="three-quarters" placeholder="Street" autocorrect="off" />
-                <input type="text" name="cc_street2" id="address2" className="quarter" placeholder="Apt/Suite" autocorrect="off" /> 
-                <input type="text" name="cc_city" id="city" placeholder="City" autocorrect="off" />
-                <input type="text" name="cc_state" id="region" placeholder="State/Provinc" autocorrect="off" />
-                <input type="text" name="cc_postal_code" id="postal" placeholder="Zip Code" autocorrect="off" />
-                <select name="cc_country" id="country" x-autocompletetype="country">
-                </select>
-                </fieldset>
-                    <button>Complete My Order!</button>
-                </form>
-                </section>
-            </section>
-        </section>
-        </body> */}
