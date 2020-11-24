d5b8d626dbb8d875822d91bfa42e69a54d2622f9
Jorge Abdo <jorgeabdo@Jorges-MacBook-Pro.local>
2020-11-24 12:57:26 -0600

    table

--- src/pages/ordenes.jsx
+++ src/pages/ordenes.jsx
@@ -1,34 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
-
-const Ordenes = () => {
-    return ( 
-        <React.Fragment>
-        <div className="bs-example container" data-example-id="striped-table">
+const Ordenes = ({ Orders }) => {
+  if (Orders === undefined) {
+    return null;
+  }
+  return (
+    <React.Fragment>
+      <div className="bs-example container" data-example-id="striped-table">
         <table className="table table-striped table-bordered table-hover">
-        <caption>Ordenes de Venta</caption>
-        <thead>
-        <tr>
-        <th>#</th>
-        <th>Nombre</th>
-        <th>Apellido</th>
-        <th>Telefono</th>
-        <th>Direccion</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-        <th scope="row">1</th>
-        <td>Ricardo</td>
-        <td>Trevino</td>
-        <td>(868) 828- 2378</td>
-        <td>Av. Tamaulipas 45 C.P. 87330</td>
-        </tr>
-        </tbody>
+          <caption>Ordenes de Venta</caption>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Telefono</th>
+              <th>Direccion</th>
+              <th>Arte ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Orders &&
+              Orders.map((order, key) => (
+                <tr>
+                  <th scope="row">1</th>
+                  <td>{order.firstName}</td>
+                  <td>{order.lastName}</td>
+                  <td>{order.phone}</td>
+                  <td>{order.address}</td>
+                  <td>{order.artID}</td>
+                </tr>
+              ))}
+          </tbody>
         </table>
-        </div>
-        </React.Fragment>
-     );
-}
- 
-export default Ordenes;
+      </div>
+    </React.Fragment>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    profile: state.firebase.profile,
+    Orders: state.firestore.ordered.Orders,
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect((props) => {
+    if (props.profile.userID === undefined) return [];
+
+    return [
+      {
+        collection: "Orders",
+      },
+    ];
+  })
+)(Ordenes);
